e2c6088f05e4b57a176841c62b35258121681d28
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2023-05-12 13:22:59 +0200

    fichier vite.config modifié

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,24 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
 export default defineConfig({
 	publicDir: "public",
 	base: "./",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				src: resolve(__dirname, "src/Activision.html"),
+				src: resolve(__dirname, "src/BethesdaSoftworks.html"),
+				src: resolve(__dirname, "src/ElectronicArts.html"),
+				src: resolve(__dirname, "src/MicrosoftGameStudios.html"),
+				src: resolve(__dirname, "src/Nintendo.html"),
+				src: resolve(__dirname, "src/Sega.html"),
+				src: resolve(__dirname, "src/SonyComputerEntertainment.html"),
+				src: resolve(__dirname, "src/SquareSoft.html"),
+				src: resolve(__dirname, "src/Take-TwoInteractive.html"),
+				src: resolve(__dirname, "src/Ubisoft.html"),
+			},
+		},
+	},
 });
